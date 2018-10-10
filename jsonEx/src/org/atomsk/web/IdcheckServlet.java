f1413631e6caa97708b91be0694ab2a25f056b30
atomsk <bigman01kr@gmail.com>
2018-10-10 20:23:23 +0900

    json logincheck serverside

---
+++ jsonEx/src/org/atomsk/web/IdcheckServlet.java
@@ -0,0 +1,60 @@
+package org.atomsk.web;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class IdcheckServlet
+ */
+@WebServlet("/idCheck")
+public class IdcheckServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public IdcheckServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		
+		List<String> ids = new ArrayList<>();
+		ids.add("AAA");
+		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		response.setContentType("text/plain");
+		
+		String result =
+				ids.indexOf(request.getParameter("str")) >= 0? "NO" : "YES";
+				response.getWriter().print(result);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
